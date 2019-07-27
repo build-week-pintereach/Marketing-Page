dd7d299e71d4bc78a202ae591946dcad59ae6478
Jacob Williams <JaWilliamsDev@gmail.com>
2019-07-26 21:35:32 -0400

    finished layout, worked on style matching to home page

--- JS/about.js
+++ JS/about.js
@@ -1,17 +1,15 @@
-// Get the modal
 var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
 var modal3 = document.getElementById("myModal3");
 var modal4 = document.getElementById("myModal4");
-
-// Get the button that opens the modal
 var btn1 = document.getElementById("tim");
-
 var btn2 = document.getElementById("michael");
-
 var btn3 = document.getElementById("ken");
-
 var btn4 = document.getElementById("jacob");
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+var span4 = document.getElementsByClassName("close")[3];
 
 btn1.onclick = function() {
   modal1.style.display = "block";
@@ -43,3 +41,19 @@ window.onclick = function(event) {
     modal4.style.display = "none";
   }
 };
+
+span.onclick = function() {
+        modal1.style.display = 'none';
+    }
+
+span2.onclick = function() {
+        modal2.style.display = 'none';
+    }
+
+span3.onclick = function() {
+        modal3.style.display = 'none';
+    }
+
+span4.onclick = function() {
+        modal4.style.display = 'none';
+    }
