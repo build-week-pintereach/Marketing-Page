2625e92b4226baab3d57a5c1bd77edcb6df1ceae
krboelter <krboelter@gmail.com>
2019-07-29 18:03:16 -0600

    Added a sticky header

---
+++ main.js
@@ -0,0 +1,11 @@
+// class Object {
+//     constructor(attribute){
+//         this.link = attribute.link;
+//     }
+// };
+
+// let newObj = new Object ({
+//     link: document.querySelector(".object h1")
+// });
+
+// newObj.link.innerHTML = "String";
